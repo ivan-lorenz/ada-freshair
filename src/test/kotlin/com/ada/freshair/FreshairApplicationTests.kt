dea91766f8cb5694e70e132db04e4bfa176c0e7b
Ivan Lorenz <ivan.lorenz@adevinta.com>
2021-05-08 20:22:10 +0200

    Initial commit

---
+++ src/test/kotlin/com/ada/freshair/FreshairApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ada.freshair
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreshairApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
