ae0eea39d6d9f987dc9b157f02815fa3b67e9620
Ivan Lorenz <ivan.lorenz@adevinta.com>
2021-05-12 09:43:02 +0200

    Air quality computation fixed

--- src/main/kotlin/com/ada/freshair/infrastructure/console/AirQualityComputation.kt
+++ src/main/kotlin/com/ada/freshair/infrastructure/console/AirQualityComputation.kt
@@ -1,19 +1,15 @@
 package com.ada.freshair.infrastructure.console
 
-import arrow.core.getOrElse
-import arrow.core.sequenceEither
 import com.ada.freshair.domain.CityAirQualityService
 import com.ada.freshair.infrastructure.City
 
 class AirQualityComputation(
     private val cityAirQualityService: CityAirQualityService
 ) {
-    fun compute(cities: List<String>) {
+    fun compute(cities: List<String>) =
         cities
             .map { City.fromParameter(it) }
             .map { cityAirQualityService.averageIndex(it) }
-            .sequenceEither()
-            .getOrElse { listOf() }
+            .mapNotNull { either -> either.fold({ null }, { it }) }
             .forEach { println("${it.cityName} average air quality index forecast is ${it.index}") }
-    }
 }
