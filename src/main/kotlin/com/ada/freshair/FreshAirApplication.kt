c54aa857fd728166402deeed49f3339187a96d13
Ivan Lorenz <ivan.lorenz@adevinta.com>
2021-05-14 09:10:05 +0200

    Initial city air quality features

---
+++ src/main/kotlin/com/ada/freshair/FreshAirApplication.kt
@@ -0,0 +1,33 @@
+package com.ada.freshair
+
+import com.ada.freshair.domain.AirQualityForecastService
+import com.ada.freshair.domain.CityAirQualityService
+import com.ada.freshair.domain.CityGeoCodingService
+import com.ada.freshair.infrastructure.api.OWMAirQualityForecastService
+import com.ada.freshair.infrastructure.api.OWMCityGeoCodingService
+import com.ada.freshair.infrastructure.console.AirQualityComputation
+import java.net.URL
+
+fun main(args: Array<String>) {
+
+    val baseUrl = URL("http://api.openweathermap.org")
+    val apiKey = System.getenv("OWM_APIKEY")
+
+    val cityGeoCodingService: CityGeoCodingService = OWMCityGeoCodingService(baseUrl, apiKey)
+    val airQualityForecastService: AirQualityForecastService = OWMAirQualityForecastService(baseUrl, apiKey)
+    val cityAirQualityService = CityAirQualityService(cityGeoCodingService, airQualityForecastService)
+    val airQualityComputation = AirQualityComputation(cityAirQualityService)
+
+    val arguments = args.fold(Pair(emptyMap<String, List<String>>(), "")) { (map, lastKey), elem ->
+        if (elem.startsWith("-"))  Pair(map + (elem to emptyList()), elem)
+        else Pair(map + (lastKey to map.getOrDefault(lastKey, emptyList()) + elem), lastKey)
+    }.first
+
+    for (argument in arguments.keys) {
+        val cities = arguments[argument] ?: emptyList()
+        when (argument) {
+            "--city" -> airQualityComputation.compute(cities)
+        }
+    }
+}
+
---
+++ src/main/kotlin/com/ada/freshair/infrastructure/console/AirQualityComputation.kt
@@ -0,0 +1,15 @@
+package com.ada.freshair.infrastructure.console
+
+import com.ada.freshair.domain.CityAirQualityService
+import com.ada.freshair.infrastructure.City
+
+class AirQualityComputation(
+    private val cityAirQualityService: CityAirQualityService
+) {
+    fun compute(cities: List<String>) {
+        cities
+            .map { City.fromParameter(it) }
+            .map { cityAirQualityService.averageIndex(it) }
+            .forEach { println("${it.cityName} average air quality index forecast is ${it.index}") }
+    }
+}
